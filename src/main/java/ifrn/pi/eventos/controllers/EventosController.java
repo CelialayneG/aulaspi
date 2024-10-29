6c6d06bbe5e831f7eefbf921cab71c09b177f68b
Célia Layne Gonçalves Miranda <20211204010008@ifrn.local>
2024-10-29 10:12:16 -0300

    Configurando classe de modelo Evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -3,11 +3,19 @@ package ifrn.pi.eventos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.eventos.models.Evento;
+
 @Controller
 public class EventosController {
 
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+	
+	@RequestMapping("/eventos/form/enviar")
+	public String enviar(Evento evento) {
+		System.out.println("Os dados foram enviados foram: " + evento.getNome() + ", " + evento.getLocal() + ", " + evento.getData() + ", " + evento.getHorario());
+		return "enviar";
 	}
 }
