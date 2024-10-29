f3aa4a570ea32939b05fdbc13b86a046cb861af7
Célia Layne Gonçalves Miranda <20211204010008@ifrn.local>
2024-10-29 10:31:28 -0300

    Enviando dados do formulário para a aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -2,6 +2,7 @@ package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import ifrn.pi.eventos.models.Evento;
 
@@ -17,5 +18,13 @@ public class EventosController {
 	public String enviar(Evento evento) {
 		System.out.println("Os dados foram enviados foram: " + evento.getNome() + ", " + evento.getLocal() + ", " + evento.getData() + ", " + evento.getHorario());
 		return "enviar";
+	}
+	
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+		
+		System.out.println(evento);
+		
+		return "evento-adicionado";
 	}
 }
