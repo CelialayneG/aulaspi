301816d1263ea6ec9f84117e7aeed0142b4156e5
Célia Layne Gonçalves Miranda <20211204010008@ifrn.local>
2024-10-29 10:52:24 -0300

    Criando Repository para Evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,29 +1,30 @@
 package ifrn.pi.eventos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.EventoRepository;
 
 @Controller
 public class EventosController {
+	
+	@Autowired
+	private EventoRepository er;
 
 	@RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
 	}
 	
-	@RequestMapping("/eventos/form/enviar")
-	public String enviar(Evento evento) {
-		System.out.println("Os dados foram enviados foram: " + evento.getNome() + ", " + evento.getLocal() + ", " + evento.getData() + ", " + evento.getHorario());
-		return "enviar";
-	}
 	
 	@PostMapping("/eventos")
 	public String adicionar(Evento evento) {
 		
 		System.out.println(evento);
+		er.save(evento);
 		
 		return "evento-adicionado";
 	}
